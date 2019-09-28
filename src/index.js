ca7c8d1da9d78a68b2b200f10c2ba6e13a26d15e
Cristobal Vega <cristobalvega96@gmail.com>
2019-09-28 12:18:49 -0500

    Propuesta solucion Reto 5 - Cristobal Vega

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
+localStorage.clear();
 const $app = document.getElementById('app');
 const $observe = document.getElementById('observe');
-const API = 'https://rickandmortyapi.com/api/character/';
+const API = 'https://us-central1-escuelajs-api.cloudfunctions.net/characters';
 
 const getData = api => {
   fetch(api)
     .then(response => response.json())
     .then(response => {
       const characters = response.results;
+      // debugger
+      const next_fetch = response.info.next;
+      localStorage.setItem("next_fetch",next_fetch);
+      console.log(next_fetch);
       let output = characters.map(character => {
         return `
       <article class="Card">
@@ -23,8 +28,13 @@ const getData = api => {
     .catch(error => console.log(error));
 }
 
-const loadData = () => {
-  getData(API);
+const loadData = async () => {
+  // getData(API);
+  if(localStorage.getItem("next_fetch")) {
+    await getData(localStorage.getItem("next_fetch"))
+  } else {
+    await getData(API);
+  }
 }
 
 const intersectionObserver = new IntersectionObserver(entries => {
